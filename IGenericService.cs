0872ebb3d280d5e0009f31b8471aaecea0049c1c
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-18 17:47:11 +0300

    Add files via upload

---
+++ IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLayer.Abstract
+{
+    //GenericService dışardan bir T değeri alacak(entity)
+    //her bir entity için bu kısım ayrı ayrı yapılabilir ama bu kolaylaştırıyor.
+     public interface IGenericService<T>
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T GetByID(int id);
+    }
+}
