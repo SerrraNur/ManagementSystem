0f6e75fcb42fc275aaa25bb784b2acd07e5cea43
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-18 17:26:44 +0300

    Add files via upload

---
+++ IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T:class
+    {
+        //Tanımlayacağım metotların isimlerini yazıyorum.
+        //Crud işlemlerine ait memottları tutacak olan kısım burası
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+        T GetById(int id);
+    }
+}
