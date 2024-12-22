a033c8b7a6cbdfcc56f3aeb941ef302408c2c480
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-22 20:43:01 +0300

    Update LoginViewModel.cs

--- Model/LoginViewModel.cs
+++ Model/LoginViewModel.cs
@@ -1,13 +1,15 @@
-﻿using EntityLayer.Concrete;
+using EntityLayer.Concrete;
 
 namespace Core_ManagementSystem.Models
 {
     public class LoginViewModel
     {
         public Login Login { get; set; }
-        public User User { get; set; }
-        public Role Role { get; set; }
+        public User User_Name { get; set; }
+        public Role Role_ID { get; set; }
         public Student Student { get; set; }
         public Advisor Advisor { get; set; }
+        public string Password { get; set; }
     }
 }
+
