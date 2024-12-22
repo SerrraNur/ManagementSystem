b8b145296a721b4f0ef7cc4c55db838bd3b19545
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-22 20:43:58 +0300

    Add files via upload

---
+++ Model/AdvisorModelValidator.cs
@@ -0,0 +1,14 @@
+﻿
+using FluentValidation;
+namespace Core_ManagementSystem.Models
+{
+    public class AdvisorViewModelValidator : AbstractValidator<AdvisorViewModel>
+    {
+        public AdvisorViewModelValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty().WithMessage("Danışman adı boş olamaz.")
+                .MinimumLength(3).WithMessage("Danışman adı en az 3 karakter olmalıdır.");
+        }
+    }
+}
---
+++ Model/LoginModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using EntityLayer.Concrete;
+
+
+namespace Core_ManagementSystem.Models
+{
+    public class LoginViewModelValidator : AbstractValidator<LoginViewModel>
+    {
+        public LoginViewModelValidator()
+        {
+            RuleFor(x => x.User_Name)
+                .NotEmpty().WithMessage("Username is required.")
+                .Length(3, 20).WithMessage("Username must be between 3 and 20 characters.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .Length(6, 20).WithMessage("Password must be between 6 and 20 characters.");
+        }
+    }
+}
