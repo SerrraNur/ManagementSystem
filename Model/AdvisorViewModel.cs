0d20903a3145f319d1a9bc01ef57af6242336d4f
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-22 20:42:20 +0300

    Update AdvisorViewModel.cs

--- Model/AdvisorViewModel.cs
+++ Model/AdvisorViewModel.cs
@@ -1,11 +1,25 @@
-﻿using EntityLayer.Concrete;
+using BuisnessLayer.ValidationRules;
+using Core_ManagementSystem.Models;
+using EntityLayer.Concrete;
+using FluentValidation;
 
 namespace Core_ManagementSystem.Models
 {
     public class AdvisorViewModel
     {
+        public string Email { get; set; }
+        public string FullName { get; set; }
         public Advisor Advisor { get; set; }
         public List<Student> Students { get; set; }
         public List<Course> Courses { get; set; }
+    }
+    public class AdvisorViewModelValidator : AbstractValidator<AdvisorViewModel>
+    {
+        
+        public AdvisorViewModelValidator()
+        {
+            RuleFor(x => x.FullName).NotEmpty().WithMessage("Danışman adı gereklidir.");
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Geçerli bir e-posta adresi gereklidir.");
+        }
     }
 }
