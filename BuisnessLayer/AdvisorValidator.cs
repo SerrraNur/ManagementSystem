4eeb85fac07c7dee07d8df7d127394fe9caf6a34
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-22 20:51:51 +0300

    Add files via upload

---
+++ BuisnessLayer/AdvisorValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLayer.ValidationRules
+{
+    public class AdvisorViewModel
+    {
+        public string FullName { get; set; }
+    public class AdvisorViewModelValidator:AbstractValidator<Advisor>
+    {
+        //ctor diyip enter yapınca geliyo,nasıl olduğunu tam anlamadım?
+        public AdvisorViewModelValidator()
+        {
+            RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Danışman adı boş olamaz.")
+            .MinimumLength(3).WithMessage("Danışman adı en az 3 karakter olmalıdır.");
+
+
+        }
+    }
+}
---
+++ BuisnessLayer/LoginValidator.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLayer.ValidationRules
+{
+    public class LoginViewModel
+    {
+        public string User_Name { get; set; }
+        public string PasswordHash { get; set; }
+    }
+
+    public class LoginViewModelValidator:AbstractValidator<Login>
+    {
+        public LoginViewModelValidator()
+        {
+            RuleFor(x => x.User_Name).NotEmpty().WithMessage("Kullanıcı adı boş olamaz.");
+            RuleFor(x => x.PasswordHash).NotEmpty().WithMessage("Şifre boş olamaz.");
+
+    //        RuleFor(x => x.PasswordHash)
+    //.Equal(x => x.ConfirmPassword)
+    //.WithMessage("Şifreler birbirini tutmuyor.");
+
+        }
+    }
+}
