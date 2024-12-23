23b898e26ef93779a5172eced5556787646525c8
Serra Nur Durmuş <serranurdurmus53@gmail.com>
2024-12-23 18:59:27 +0300

    Add files via upload

---
+++ Controller/AdvisorApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core_ManagementSystem.Models;
+using EntityLayer.Concrete;
+
+namespace Core_ManagementSystem.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvisorApiController : ControllerBase
+    {
+        private readonly DbContext _context;
+
+        public AdvisorApiController(DbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddAdvisor([FromBody] AdvisorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Veritabanına ekleme işlemi burada yapılır
+                var advisor = new Advisor
+                {
+                    FullName = model.FullName,
+                    Email = model.Email
+                };
+                _context.Advisors.Add(advisor);
+                _context.SaveChanges();
+
+                return Ok(new { message = "Danışman başarıyla eklendi." });
+            }
+
+            return BadRequest("Geçersiz veri.");
+        }
+    }
+}
